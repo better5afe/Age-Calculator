f58a3c421d46cdb5b460ff9c1dade708f04c96e1
better5afe <abramczyk2704@gmail.com>
2023-09-12 20:37:21 +0200

    calculator result styling for mobile view done

--- src/components/CalculatorResult.tsx
+++ src/components/CalculatorResult.tsx
@@ -1,9 +1,19 @@
-import './CalculatorResult.scss'
+import './CalculatorResult.scss';
 
 const CalculatorResult = () => {
-    return <section>
-        <h1>result</h1>
-    </section>
-}
+	return (
+		<section className='calculator__result'>
+			<p className='calculator__result-output'>
+				<span className='calculator__result-output--colored'>38</span> years
+			</p>
+			<p className='calculator__result-output'>
+				<span className='calculator__result-output--colored'>3</span> months
+			</p>
+			<p className='calculator__result-output'>
+				<span className='calculator__result-output--colored'>26</span> days
+			</p>
+		</section>
+	);
+};
 
-export default CalculatorResult
+export default CalculatorResult;
