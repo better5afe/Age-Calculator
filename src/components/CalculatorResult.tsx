e00ce54eca7b60d6b4458cb4aa44cfd468d65ee3
better5afe <abramczyk2704@gmail.com>
2023-09-12 21:06:52 +0200

    added animated numbers

--- src/components/CalculatorResult.tsx
+++ src/components/CalculatorResult.tsx
@@ -1,16 +1,50 @@
 import './CalculatorResult.scss';
+import AnimatedNumbers from 'react-animated-numbers';
 
 const CalculatorResult = () => {
 	return (
 		<section className='calculator__result'>
 			<p className='calculator__result-output'>
-				<span className='calculator__result-output--colored'>38</span> years
+				<span className='calculator__result-output--colored'>
+					<AnimatedNumbers
+						animateToNumber={38}
+						configs={[
+							{ mass: 1, tension: 140, friction: 126 },
+							{ mass: 1, tension: 130, friction: 114 },
+							{ mass: 1, tension: 150, friction: 112 },
+							{ mass: 1, tension: 130, friction: 120 },
+						]}
+					/>
+				</span>{' '}
+				years
 			</p>
 			<p className='calculator__result-output'>
-				<span className='calculator__result-output--colored'>3</span> months
+				<span className='calculator__result-output--colored'>
+					<AnimatedNumbers
+						animateToNumber={3}
+						configs={[
+							{ mass: 1, tension: 140, friction: 126 },
+							{ mass: 1, tension: 130, friction: 114 },
+							{ mass: 1, tension: 150, friction: 112 },
+							{ mass: 1, tension: 130, friction: 120 },
+						]}
+					/>
+				</span>{' '}
+				months
 			</p>
 			<p className='calculator__result-output'>
-				<span className='calculator__result-output--colored'>26</span> days
+				<span className='calculator__result-output--colored'>
+					<AnimatedNumbers
+						animateToNumber={26}
+						configs={[
+							{ mass: 1, tension: 140, friction: 126 },
+							{ mass: 1, tension: 130, friction: 114 },
+							{ mass: 1, tension: 150, friction: 112 },
+							{ mass: 1, tension: 130, friction: 120 },
+                        ]}
+					/>
+				</span>{' '}
+				days
 			</p>
 		</section>
 	);
